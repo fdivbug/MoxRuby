3f7fa9cd230e35182ec55b042f2b313524fb1362
Jeff Abbott <jabbott@redhat.com>
2012-04-20 12:48:24 -0400

    Added a couple handy methods for expansion URLs.

--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,3 +1,12 @@
 class Expansion < ActiveRecord::Base
   attr_accessible :border, :name, :release_date
+
+  def scg_url
+    "http://sales.starcitygames.com/spoiler/display.php?s[#{scg_name}]=#{scg_number}&g[G1]=NM%2FM&foil=all&for=no&display=4&numpage=200&action=Show+Results"
+  end
+
+  def gatherer_url
+    "http://gatherer.wizards.com/Pages/Search/Default.aspx?output=checklist&set=[%22#{CGI.escape(name)}%22]"
+  end
+
 end
