4d3c8e1efcad27e6364b232c95962ebcf82ee875
Jeff Abbott <jabbott@redhat.com>
2012-04-20 11:26:58 -0400

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoxRuby::Application.config.secret_token = 'e21d002d8f96c2914a839b505e2516dcb4de7897ae04f5007631a987a903426924a8dc2a3bca1c7c161838816e2753285f6e14c1c616064e5fb5481991c24ca7'
